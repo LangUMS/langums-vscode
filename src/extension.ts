fa6711c6ecd89237f0a3ddabd31bae761e4ff085
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2017-05-06 18:50:26 +0300

    small fix

--- src/extension.ts
+++ src/extension.ts
@@ -7,31 +7,8 @@
 import * as vscode from 'vscode';
 import CompletionProvider from './Providers/Completion/index';
 
-const initialConfigurations = {
-	version: '0.2.0',
-	configurations: [
-		{
-			type: 'langums',
-			request: 'launch',
-			name: 'LangUMS Debug',
-			langums: '${workspaceRoot}/langums.exe',
-			map: '${workspaceRoot}/my_map.scx',
-			lang: '${workspaceRoot}/my_map.l',
-			dst: '${workspaceRoot}/my_map_release.scx'
-		}
-	]
-};
-
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('langums', new CompletionProvider(context), '.', '\"'));
-
-	context.subscriptions.push(vscode.commands.registerCommand('extension.langums.provideInitialConfigurations', () => {
-		return [
-			'// Use IntelliSense to learn about possible Mock debug attributes.',
-			'// Hover to view descriptions of existing attributes.',
-			JSON.stringify(initialConfigurations, null, '\t')
-		].join('\n');
-	}));
 }
 
 export function deactivate() {
